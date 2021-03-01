664abc718d21b0cfa1133839ef30bc98d14bdc45
Christina Ashworth <christina.ashworth@ymail.com>
2021-03-01 10:16:15 -0600

    got rolling dice mostly working--except broken bit of code

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,33 @@
 // Put your code here
+
+console.log("Let's roll some dice, baby!")
+console.log("---------------------------")
+
+const Die = (dieValue) => {
+  return {
+    value: dieValue
+  }
+}
+
+const Roll = () => {
+  const getRandomInt = (min, max) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1))
+  }
+  const dieValue = getRandomInt(1, 6)
+  const die = Die(dieValue)
+  return die
+}
+
+for (let i = 0; i < 10; i++) {
+  die1 = Roll()
+  die2 = Roll();
+
+  let message = `${die1} + ${die2} === ${die1.value} + ${die2.value}`
+  if (die1.value === die2.value) {
+    message += " DOUBLES!"
+  }
+
+  console.log(message)
+}
